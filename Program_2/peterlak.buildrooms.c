1a11beda88e612140cf9ee33d3c444bbb92f320c
LakePeterson <9petersonlake9@gmail.com>
2020-01-29 14:31:12 -0800

    Create Directory and File Path Functions

--- Program_2/peterlak.buildrooms.c
+++ Program_2/peterlak.buildrooms.c
@@ -7,13 +7,52 @@
 // Output:
 ////////////////////////////////////////////////////////////////////////
 
-#include <conio.h>
-#include <dir.h>
-#include <process.h>
 #include <stdio.h>
+#include <string.h>
 
+int createDirectory();
+void createRooms(char*);
 
-void main()
+int main()
 {
-  
+  createDirectory();
+
+
+
+  return 0;
+}
+
+int createDirectory()
+{
+  int getPID = getpid();
+  char stringPID[50];
+  char directory[100] = "peterlak.buildrooms.";
+
+  sprintf(stringPID, "%d", getPID);
+  strcat(directory, stringPID);
+
+  int valid = mkdir(directory, 0700);
+
+  if(valid == 0)
+  {
+    createRooms(directory);
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void createRooms(char* directory)
+{
+  char filePath[100];
+  sprintf(filePath, "./%s/livingRoom", directory);
+
+  printf("%s\n", filePath);
+
+  FILE* outFile = fopen(filePath, "w");
+
+  fclose(outFile);
+
 }
